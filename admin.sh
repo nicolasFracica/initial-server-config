5bc36ec9f90f32b4764b6c9b2795b2bf9174dfb0
nicolasFracica <48388706+nicolasFracica@users.noreply.github.com>
2021-09-02 11:33:48 -0500

    Instalación de Docker

--- admin.sh
+++ admin.sh
@@ -26,8 +26,18 @@ sudo ln -s $var1 /usr/bin/npm
 var1=$(which nodemon)
 sudo ln -s $var1 /usr/bin/nodemon
 
+# Instalar Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
 # Instalar Nginx y abrir puertos HTTP y HTTPS
 sudo apt update
 sudo apt install nginx
 sudo ufw allow 80
 sudo ufw allow 443
+
+# Seguir instalando Docker
+sudo apt install docker-ce
+sudo apt install docker-compose
+sudo usermod -aG docker ${USER}
+
