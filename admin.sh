200027118e04cc4cc6a7989bccbdf5a4d5e484d0
nicolasFracica <48388706+nicolasFracica@users.noreply.github.com>
2022-06-27 21:13:14 -0500

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -3,11 +3,11 @@
 
 # Instalar Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 
 # Instalar Nginx y abrir puertos HTTP y HTTPS
 sudo apt update
-sudo apt install nginx
+sudo apt install nginx -y
 sudo ufw enable -y
 sudo ufw allow OpenSSH
 sudo ufw allow 80
